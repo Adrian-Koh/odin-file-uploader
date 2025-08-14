583a6d65a2e37c895e29ca4cf5aa5fd8ebaa4adc
Adrian-Koh <adriankoh216@yahoo.com>
2025-08-14 15:31:02 +0800

    Implement edit and delete folder functionality

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -9,6 +9,7 @@ function newFolderGet(req, res) {
     title: "New folder",
     formName: "folder",
     links,
+    postLink: "/folder/new",
   });
 }
 
@@ -53,8 +54,80 @@ async function folderGet(req, res) {
   res.render("folder", { folder, files, links });
 }
 
+async function folderDeleteGet(req, res, next) {
+  const { folderId } = req.params;
+  const prisma = new PrismaClient();
+  const files = await prisma.file.deleteMany({
+    where: {
+      folderId: parseInt(folderId),
+    },
+  });
+  const folder = await prisma.folder.delete({
+    where: {
+      id: parseInt(folderId),
+    },
+  });
+  res.redirect("/");
+}
+
+async function editFolderGet(req, res, next) {
+  const { folderId } = req.params;
+  const prisma = new PrismaClient();
+  const folder = await prisma.folder.findUnique({
+    where: {
+      id: parseInt(folderId),
+    },
+  });
+  res.render("formContainer", {
+    title: "Update folder",
+    formName: "folder",
+    links,
+    postLink: "/folder/edit/" + folderId,
+    folderName: folder.name,
+  });
+}
+
+async function editFolderPost(req, res, next) {
+  try {
+    const { folder } = req.body;
+    const { folderId } = req.params;
+
+    if (fs.existsSync(path.join(DOWNLOAD_PATH, folder))) {
+      throw new Error(
+        `Folder ${path.join(DOWNLOAD_PATH, folder)} already exists.`
+      );
+    }
+
+    const prisma = new PrismaClient();
+    const originalFolder = await prisma.folder.findUnique({
+      where: {
+        id: parseInt(folderId),
+      },
+    });
+    const createdFolder = await prisma.folder.update({
+      where: {
+        id: parseInt(folderId),
+      },
+      data: {
+        name: folder,
+      },
+    });
+
+    fs.renameSync(
+      path.join(DOWNLOAD_PATH, originalFolder.name),
+      path.join(DOWNLOAD_PATH, folder)
+    );
+    res.redirect("/");
+  } catch (err) {
+    next(err);
+  }
+}
+
 module.exports = {
   newFolderGet,
   newFolderPost,
   folderGet,
+  folderDeleteGet,
+  editFolderGet,
+  editFolderPost,
 };
--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -6,5 +6,20 @@ const { isAuthenticated } = require("../lib/authMiddleware");
 folderRouter.get("/new", isAuthenticated, folderController.newFolderGet);
 folderRouter.post("/new", isAuthenticated, folderController.newFolderPost);
 folderRouter.get("/:folderId", isAuthenticated, folderController.folderGet);
+folderRouter.get(
+  "/delete/:folderId",
+  isAuthenticated,
+  folderController.folderDeleteGet
+);
+folderRouter.get(
+  "/edit/:folderId",
+  isAuthenticated,
+  folderController.editFolderGet
+);
+folderRouter.post(
+  "/edit/:folderId",
+  isAuthenticated,
+  folderController.editFolderPost
+);
 
 module.exports = folderRouter;
